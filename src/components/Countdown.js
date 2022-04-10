040b997a79f0581756d95d0a067e99bb57925308
murichristopher <murichristopher@outlook.com>
2022-04-10 08:51:06 -0300

    Erro-1 solved

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,31 +1,31 @@
 import React from "react";
 import Timer from "../Timer";
-import TimerElement from "./TimerElement"
+import TimerElement from "./TimerElement";
 
 function Countdown() {
   const countDownDate = new Date("Apr,29 2023 23:19:02").getTime();
   const [countdownTimer, setCountdownTimer] = React.useState({});
-  const [titlePage, setTitlePage] = React.useState("Titulo estático")
+  // const [titlePage, setTitlePage] = React.useState("Titulo estático")
 
-  function advanceCountDown(){
-      setInterval(() => {
-        setCountdownTimer(Timer(countDownDate))
-      }, 1000)
+  function advanceCountDown() {
+    setInterval(() => {
+      setCountdownTimer(Timer(countDownDate));
+    }, 1000);
   }
 
-  if(countdownTimer !== false){
-    advanceCountDown()
+  if (countdownTimer !== false) {
+    advanceCountDown();
   }
 
   return (
     <div className="Countdown">
-      <h1>{titlePage}</h1>
+      <h1>Teste</h1>
       <hr />
       <div className="countdown-container">
-        <TimerElement type="days"  countdownTimer={countdownTimer}/>
-          <TimerElement type="hours"  countdownTimer={countdownTimer}/>
-            <TimerElement type="minutes"  countdownTimer={countdownTimer}/>
-              <TimerElement type="seconds"  countdownTimer={countdownTimer}/>
+        <TimerElement type="days" countdownTimer={countdownTimer} />
+        <TimerElement type="hours" countdownTimer={countdownTimer} />
+        <TimerElement type="minutes" countdownTimer={countdownTimer} />
+        <TimerElement type="seconds" countdownTimer={countdownTimer} />
       </div>
     </div>
   );
